58442d5dc66e431ae9372d346de5a5796b2645cf
CarlosVictor01 <c.victorsilva2050@gmail.com>
2023-02-10 22:26:08 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="align-center">
+      <h1>{{ titulo }}</h1>
+    </div>
+    <div>
+      <ul>
+        <li v-for="name in names">
+          {{ names }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      titulo: 'POKEMONS',
+      names: []
+    }
+  }, 
+
+  created() {
+    this.$http.get('https://pokeapi.co/api/v2/pokemon/')
+      .then(res => res.json())
+      .then(name => this.names = name);
+  }
+}
+</script>
+
+<style>
+</style>
